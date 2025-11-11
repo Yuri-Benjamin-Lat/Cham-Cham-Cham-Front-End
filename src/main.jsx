aa892371269e3aafe4418b4b49b786b706e23f36
yuri Lat <yurilat3@gmail.com>
2025-11-11 20:35:19 +0800

    added protected route

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,19 @@ import './css/index.css'
 
 import Login from './pages/Login.jsx'
 import Home from './pages/Home.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
